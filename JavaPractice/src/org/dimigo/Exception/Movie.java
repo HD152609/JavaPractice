598723306cd117162c3201d0ca3e72c5d2a564b8
kimjuchan <kimjuchan@218.38.137.28>
2017-06-02 09:37:19 +0900

    adf

--- JavaPractice/src/org/dimigo/Exception/Movie.java
+++ JavaPractice/src/org/dimigo/Exception/Movie.java
@@ -39,7 +39,8 @@ public class Movie {
 public void buyTicket(int age) throws Exception{
 	try {
 	if(age<limitAge){
-			throw new Exception(title +"은/는" +limitAge+"세 이상 관람가입니다.");
+			throw new Exception(title +"은/는" +limitAge+"세 이상 관람가입니다.    "
+					+ "");
 		} 
 	else System.out.println(title+"즐감하세요");
 	
--- JavaPractice/src/org/dimigo/Exception/MovieTest.java
+++ JavaPractice/src/org/dimigo/Exception/MovieTest.java
@@ -23,8 +23,8 @@ public class MovieTest {
 	public static void main(String[] args) {
 			Movie[] movies={
 					new Movie("에이리언:커버넌트",15),
-					new Movie("가디언즈 오브 갤럭시",12)
-			};
+					new Movie("가디언즈 오브 갤럭시",12    )
+			};     
 			int age =13;
 				for(Movie movie : movies){
 					try{
